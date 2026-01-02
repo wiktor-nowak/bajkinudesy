11c5b030d61d107a8dbc2cb492fcf61d346f9105
wiktor-nowak <nowak.wiktor95@gmail.com>
2026-01-02 19:18:30 +0100

    2nd edition changes fix

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,7 +7,7 @@ function List() {
         <h4 className="text-3xl text-columbia-blue">Co gwarantujemy?</h4>
         <div className="w-full h-[2px] bg-columbia-blue rounded-full my-6 md:hidden" />
         <ul className="flex flex-col gap-[16px] justify-center items-start md:mt-8">
-          <li>✔ Minimum cztery dopracowane sety tematyczne z rekwizytami</li>
+          <li>✔ Minimum trzy dopracowane sety tematyczne z rekwizytami</li>
           <li>✔ Profesjonalne oświetlenie studyjne by FilmBeast</li>
           <li>✔ Przekąski i napoje w trakcie wydarzenia</li>
           <li>✔ Wsparcie organizacyjne podczas całego spotkania</li>
